3b19cd1f93b068dafb14b636095d71a11991433f
ysb4642 <ysb4642@naver.com>
2022-08-16 17:43:36 +0900

    관리자 상품 리스트, 상품등록, 엑셀파일로 다운

--- csms/src/main/java/com/spring/csms/admin/goods/controller/AdminGoodsController.java
+++ csms/src/main/java/com/spring/csms/admin/goods/controller/AdminGoodsController.java
@@ -2,9 +2,23 @@ package com.spring.csms.admin.goods.controller;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor.HSSFColorPredefined;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,6 +26,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
@@ -95,6 +110,102 @@ public class AdminGoodsController {
 		httpHeaders.add("Content-Type", "text/html; charset=utf-8");
 		
 		return new ResponseEntity<Object>(jsScript, httpHeaders, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/goodsExcelExport" , method=RequestMethod.GET)
+	public void goodsExcelExport(HttpServletResponse response , @RequestParam Map<String, String> dateMap) throws Exception {
+		  
+		SimpleDateFormat fileSdf = new SimpleDateFormat("yyyy_MM_dd_hh_mm");
+		SimpleDateFormat dateSdf = new SimpleDateFormat("yyyy-MM-dd");
+		String makeFileTime = fileSdf.format(new Date());
+		String makeFileName = makeFileTime + "_goodsList.xls";
+		
+	    // 워크북 생성
+	    Workbook wb = new HSSFWorkbook();
+	    Sheet sheet = wb.createSheet("상품리스트");
+	    Row row = null;
+	    Cell cell = null;
+
+	    int rowNo = 0;
+
+	    // 테이블 헤더용 스타일
+	    CellStyle headStyle = wb.createCellStyle();
+	    // 가는 경계선
+	    headStyle.setBorderTop(BorderStyle.THIN);
+	    headStyle.setBorderBottom(BorderStyle.THIN);
+	    headStyle.setBorderLeft(BorderStyle.THIN);
+	    headStyle.setBorderRight(BorderStyle.THIN);
+
+	    // 노란색 배경
+	    headStyle.setFillForegroundColor(HSSFColorPredefined.YELLOW.getIndex());
+	    headStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+	    
+	    // 가운데 정렬
+	    headStyle.setAlignment(HorizontalAlignment.CENTER);
+
+	    // 데이터용 경계 스타일 테두리만 지정
+	    CellStyle bodyStyle = wb.createCellStyle();
+	    bodyStyle.setBorderTop(BorderStyle.THIN);
+	    bodyStyle.setBorderBottom(BorderStyle.THIN);
+	    bodyStyle.setBorderLeft(BorderStyle.THIN);
+	    bodyStyle.setBorderRight(BorderStyle.THIN);
+
+	    // 헤더 생성
+	    row = sheet.createRow(rowNo++);
+	    cell = row.createCell(0);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("상품번호");
+	    cell = row.createCell(1);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("상품이름");
+	    cell = row.createCell(2);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("브랜드");
+	    cell = row.createCell(3);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("상품가격");
+	    cell = row.createCell(4);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("중량");
+	    cell = row.createCell(5);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("유통기한");
+	    cell = row.createCell(6);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("등록일자");
+
+		for (GoodsDto goodsDto :  adminGoodsService.getGoodsList()) {
+			row = sheet.createRow(rowNo++);
+	        cell = row.createCell(0);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(goodsDto.getGoodsCd());
+	        cell = row.createCell(1);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(goodsDto.getGoodsNm());
+	        cell = row.createCell(2);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(goodsDto.getBrand());
+	        cell = row.createCell(3);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(goodsDto.getPrice());
+	        cell = row.createCell(4);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(goodsDto.getCapacity());
+	        cell = row.createCell(5);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(goodsDto.getExpiryDate());
+	        cell = row.createCell(6);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(dateSdf.format(goodsDto.getEnrollDt()));
+		} 
+
+	    response.setContentType("ms-vnd/excel");
+	    response.setHeader("Content-Disposition", "attachment;filename=" + makeFileName);
+
+	    // 엑셀 출력
+	    wb.write(response.getOutputStream());
+	    wb.close();
+		
 	}
 	
 	
--- csms/src/main/java/com/spring/csms/common/controller/CommonController.java
+++ csms/src/main/java/com/spring/csms/common/controller/CommonController.java
@@ -1,8 +1,13 @@
 package com.spring.csms.common.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.coobird.thumbnailator.Thumbnails;
 
 
 @Controller
@@ -37,4 +45,47 @@ public class CommonController {
 		return "/index";
 	}
 	
+	
+	private static final String CURR_IMAGE_REPO_PATH = "/Users/ysb/file_repo";
+	private static final String SEPERATOR = "/";	// mac
+	
+//	private static final String CURR_IMAGE_REPO_PATH = "C:\\file_repo";
+//	private static final String SEPERATOR = "\\";	// window
+
+//	private static final String CURR_IMAGE_REPO_PATH = "/var/lib/tomcat8/file_repo";
+//	private static final String SEPERATOR = "/";		// linux
+	
+	@RequestMapping(value = "/thumbnails", method = RequestMethod.GET)
+	public void thumbnails(@RequestParam("goodsFileName") String goodsFileName, HttpServletResponse response) throws IOException {
+			
+		OutputStream out = response.getOutputStream();
+		String filePath = CURR_IMAGE_REPO_PATH + SEPERATOR + goodsFileName;
+		
+		File image = new File(filePath);
+		if (image.exists()) { 
+			Thumbnails.of(image).size(800,800).outputFormat("png").toOutputStream(out);
+		}
+		byte[] buffer = new byte[1024 * 8];
+		out.write(buffer);
+		out.close();
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
